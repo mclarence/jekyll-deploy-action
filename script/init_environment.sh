8d29d6521308b4cc3fcfa98702fdaaaaf2938acc
Jeffrey Tse <jeffreytse.mail@gmail.com>
2024-03-29 09:55:23 +0800

    test

--- script/init_environment.sh
+++ script/init_environment.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Generate a default secret key
+# To prevent archlinux-keyring from no secret key available to sign with
+pacman-key --init
+
 # Update packages database
 pacman -Syu --noconfirm
 
@@ -11,11 +15,31 @@ if [[ -n "${SSH_PRIVATE_KEY}" ]]; then
   pacman -S --noconfirm openssh
 fi
 
-# Installing ruby libraries
-pacman -S --noconfirm ruby2.7 ruby-bundler
+# Install asdf-vm
+if [[ ! -f "${WORKING_DIR}/.asdf/asdf.sh" ]]; then
+  git clone https://github.com/asdf-vm/asdf.git \
+      ${WORKING_DIR}/.asdf --branch v0.14.0
+fi
 
-# Setting default ruby version
-cp /usr/bin/ruby-2.7 /usr/bin/ruby
+# Fix invalid cache to asdf tools' installation
+ln -s ${WORKING_DIR}/.asdf ${HOME}/.asdf
+ln -s ${WORKING_DIR}/.asdf/.tool-versions ${HOME}/.tool-versions
+
+source ${HOME}/.asdf/asdf.sh
+realpath ${HOME}
+ls -la ${HOME}/.asdf
+
+# Install ruby environment
+pacman -S --noconfirm libyaml
+
+if ! asdf list ruby ${RUBY_VER} &>/dev/null; then
+  asdf plugin add ruby
+  asdf install ruby ${RUBY_VER}
+fi
+
+asdf global ruby ${RUBY_VER}
+
+ls -la ${HOME}
 
 # debug
 ruby -v && bundle version
